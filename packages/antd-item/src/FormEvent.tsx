0b5224240a040e16d2efa8de90c62c86646c5ebd
levi <cgfeel@gmail.com>
2026-01-12 04:56:52 +0800

    fix: up form init

--- packages/antd-item/src/FormEvent.tsx
+++ packages/antd-item/src/FormEvent.tsx
@@ -20,7 +20,7 @@ const FormProviderInner: FC<PropsWithChildren<FormEventContextInstance>> = ({
 
 const FormProvider = memo(FormProviderInner);
 
-// 初始表单
+// 初始表单-1
 const FormInitialization = <
   ValuesType,
   Name extends string,
